a569a1d7ef47bdd860a725002be4af50a06fbed0
think <serhou@163.com>
2016-05-27 23:02:04 +0800

    单元测试Junit测试Dao中方法

---
+++ src/test/java/org/seckill/dao/SuccessKilledDaoTest.java
@@ -0,0 +1,39 @@
+package org.seckill.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.entity.SuccessKilled;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by think on 2016-05-27-0027.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/spring-dao.xml")
+public class SuccessKilledDaoTest {
+
+    @Resource
+    private SuccessKilledDao successKilledDao;
+
+    @Test
+    public void insertSuccessKilled() throws Exception {
+        long id = 1001L;
+        long phone = 17734758293L;
+        int insertCount = successKilledDao.insertSuccessKilled(id, phone);
+        System.out.println("insertCount = " + insertCount);
+    }
+
+    @Test
+    public void queryByIdWithSeckill() throws Exception {
+        long id = 1001L;
+        long phone = 17734758293L;
+        SuccessKilled successKilled = successKilledDao.queryByIdWithSeckill(id, phone);
+        System.out.println(successKilled);
+        System.out.println(successKilled.getSeckill());
+    }
+}
