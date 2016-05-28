8427680fda27be857b7ca14177328fddbc9b77ae
think <serhou@163.com>
2016-05-28 09:54:52 +0800

    业务接口的实现+使用枚举表示数据字典

--- src/main/java/org/seckill/dto/SeckillExecution.java
+++ src/main/java/org/seckill/dto/SeckillExecution.java
@@ -1,6 +1,7 @@
 package org.seckill.dto;
 
 import org.seckill.entity.SuccessKilled;
+import org.seckill.enums.SeckillStateEnum;
 
 /**
  * 封装秒杀执行后的结果
@@ -19,16 +20,16 @@ public class SeckillExecution {
     //秒杀成功对象
     private SuccessKilled successKilled;
 
-    public SeckillExecution(long seckillId, int state, String stateInfo, SuccessKilled successKilled) {
+    public SeckillExecution(long seckillId, SeckillStateEnum seckillStateEnum, SuccessKilled successKilled) {
         this.seckillId = seckillId;
-        this.state = state;
-        this.stateInfo = stateInfo;
+        this.state = seckillStateEnum.getState();
+        this.stateInfo = seckillStateEnum.getStateInfo();
         this.successKilled = successKilled;
     }
 
-    public SeckillExecution(long seckillId, int state, String stateInfo) {
+    public SeckillExecution(long seckillId, SeckillStateEnum seckillStateEnum) {
         this.seckillId = seckillId;
-        this.state = state;
-        this.stateInfo = stateInfo;
+        this.state = seckillStateEnum.getState();
+        this.stateInfo = seckillStateEnum.getStateInfo();
     }
 }
--- src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -5,6 +5,8 @@ import org.seckill.dao.SuccessKilledDao;
 import org.seckill.dto.Exposer;
 import org.seckill.dto.SeckillExecution;
 import org.seckill.entity.Seckill;
+import org.seckill.entity.SuccessKilled;
+import org.seckill.enums.SeckillStateEnum;
 import org.seckill.exception.RepeatKillException;
 import org.seckill.exception.SeckillCloseException;
 import org.seckill.exception.SeckillException;
@@ -62,6 +64,38 @@ public class SeckillServiceImpl implements SeckillService {
 
     public SeckillExecution executteSeckill(long seckillId, long uesrPhone, String md5)
             throws SeckillException, RepeatKillException, SeckillCloseException {
-        return null;
+        if(md5 == null || md5.equals(getMD5(seckillId))){
+            throw new SeckillException("seckill data rewrite");
+        }
+        //执行秒杀逻辑：减库存+记录购买行为
+        Date nowTime = new Date();
+        try {
+            //减库存
+            int updateCount = seckillDao.reduceNumber(seckillId,nowTime);
+            if(updateCount <= 0){
+                //没有更新到记录
+                throw new SeckillCloseException("seckill is closed");
+            } else {
+                //记录购买行为
+                int insertCount = successKilledDao.insertSuccessKilled(seckillId,uesrPhone);
+                if(insertCount<=0){
+                    throw  new RepeatKillException("seckill repeated");
+                }else{
+                    //秒杀成功
+                    SuccessKilled successKilled = successKilledDao.queryByIdWithSeckill(seckillId,uesrPhone);
+                    return new SeckillExecution(seckillId, SeckillStateEnum.SUCCESS, successKilled);
+                }
+            }
+        }catch (SeckillCloseException close){
+            throw close;
+        }catch (RepeatKillException r){
+            throw r;
+        }catch (Exception e){
+            logger.error(e.getMessage(),e);
+            //所有编译期异常转换为运行期异常
+            throw new SeckillException("seckill inner error: " + e.getMessage());
+        }
+
+
     }
 }
